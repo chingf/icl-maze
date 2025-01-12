9f86824d5552346b3a0725d0fd4024c525d861de
Ching Fang <cfang@holygpu8a15202.rc.fas.harvard.edu>
2025-01-12 14:44:49 -0500

    Make train.py compatible with HDF5 files

--- train.py
+++ train.py
@@ -7,12 +7,13 @@ from omegaconf import DictConfig, OmegaConf
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+from pytorch_lightning import LightningDataModule
 import wandb
 wandb.login()
 
 import numpy as np
 import random
-from src.dataset import Dataset, ImageDataset
+from src.dataset import get_dataset, HDF5Dataset
 from src.utils import (
     build_env_name,
     build_model_name,
@@ -43,8 +44,11 @@ def main(cfg: DictConfig):
     model_name = build_model_name(model_config, optimizer_config)
     dataset_storage_dir = f'{cfg.storage_dir}/{cfg.wandb.project}/{env_name}/datasets'
     model_storage_dir = f'{cfg.storage_dir}/{cfg.wandb.project}/{env_name}/models/{model_name}'
-    train_dset_path = os.path.join(dataset_storage_dir, build_dataset_name(0))
-    test_dset_path = os.path.join(dataset_storage_dir, build_dataset_name(1))
+    # Check if train dataset is pickle or h5 format
+    use_h5 = os.path.exists(os.path.join(dataset_storage_dir, 'train.h5'))
+    file_suffix = '.h5' if use_h5 else '.pkl'
+    train_dset_path = os.path.join(dataset_storage_dir, 'train' + file_suffix)
+    test_dset_path = os.path.join(dataset_storage_dir, 'test' + file_suffix)
     os.makedirs(model_storage_dir, exist_ok=True)
 
     # Random seed handling 
@@ -59,18 +63,6 @@ def main(cfg: DictConfig):
     torch.backends.cudnn.benchmark = False
     np.random.seed(tmp_seed)
     random.seed(tmp_seed)
-
-    # Set up datasets and dataloaders
-    train_dataset = Dataset(train_dset_path, env_config)
-    test_dataset = Dataset(test_dset_path, env_config)
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset, optimizer_config['batch_size'], shuffle=True,
-        #num_workers=optimizer_config['num_workers'],
-    )
-    test_loader = torch.utils.data.DataLoader(
-        test_dataset, optimizer_config['batch_size'],
-        #num_workers=optimizer_config['num_workers'],
-    )
 
     # Set up logging and checkpointing
     wandb_config = {
@@ -100,7 +92,7 @@ def main(cfg: DictConfig):
     checkpoint_callback = ModelCheckpoint(
         dirpath=model_storage_dir,
         filename="{epoch}-{val_loss:.6f}",
-        save_top_k=3,
+        save_top_k=1,
         monitor='val_loss',
         mode='min',
         save_last=True
@@ -115,26 +107,8 @@ def main(cfg: DictConfig):
         verbose=True,
     )
 
-    # Debugging callback
-    class GradientDebugCallback(pl.Callback):
-        def __init__(self):
-            self.gradients_seen = set()
-            
-        def on_before_optimizer_step(self, trainer, pl_module, optimizer):
-            for name, param in pl_module.named_parameters():
-                if param.grad is not None:
-                    self.gradients_seen.add(name)
-                
-        def on_train_epoch_end(self, trainer, pl_module):
-            print("\nParameters that received gradients this epoch:")
-            all_params = set(name for name, _ in pl_module.named_parameters())
-            for name in all_params:
-                if name in self.gradients_seen:
-                    print(f"✓ {name}")
-                else:
-                    print(f"✗ {name}")
-
     # Set up trainer
+    # Set up gradient clipping
     trainer = pl.Trainer(
         max_epochs=optimizer_config['num_epochs'],
         logger=wandb_logger,
@@ -146,12 +120,72 @@ def main(cfg: DictConfig):
         precision="16-mixed",
     )
 
-    # Train model
-    trainer.fit(
-        model,
-        train_dataloaders=train_loader,
-        val_dataloaders=test_loader
-    )
+    try:
+        # Set up datasets and dataloaders
+        datamodule = DataModule(
+            train_dset_path=train_dset_path,
+            test_dset_path=test_dset_path,
+            env_config=env_config,
+            batch_size=optimizer_config['batch_size'],
+            optimizer_config=optimizer_config
+        )
+    
+        # Train model
+        trainer.fit(
+            model,
+            datamodule=datamodule,
+        )
+    finally:
+        for file in HDF5Dataset._open_files.copy():
+            try:
+                file.close()
+            except Exception as e:
+                print(f"Error closing file: {e}")
+        HDF5Dataset._open_files.clear()
+
+class DataModule(LightningDataModule):
+    def __init__(
+            self,
+            train_dset_path,
+            test_dset_path,
+            env_config,
+            batch_size,
+            optimizer_config
+    ):
+        super().__init__()
+        self.train_dset_path = train_dset_path
+        self.test_dset_path = test_dset_path
+        self.env_config = env_config
+        self.batch_size = batch_size
+        self.optimizer_config = optimizer_config
+
+    def setup(self, stage=None):
+        self.train_dataset = get_dataset(self.train_dset_path, self.env_config)
+        self.test_dataset = get_dataset(self.test_dset_path, self.env_config)
+
+    def train_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.train_dataset, self.batch_size,
+            shuffle=(not isinstance(self.train_dataset, HDF5Dataset)),
+            num_workers=self.optimizer_config['num_workers'],
+        )
+
+    def val_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.test_dataset, self.batch_size,
+            num_workers=self.optimizer_config["num_workers"],
+        )
+    
+    def teardown(self, stage=None):
+        """Clean up when training ends"""
+        if self.train_dataset:
+            if hasattr(self.train_dataset, '_file') and self.train_dataset._file is not None:
+                self.train_dataset._file.close()
+                HDF5Dataset._open_files.discard(self.train_dataset._file)
+        if self.test_dataset:
+            if hasattr(self.test_dataset, '_file') and self.test_dataset._file is not None:
+                self.test_dataset._file.close()
+                HDF5Dataset._open_files.discard(self.test_dataset._file)
 
 
 if __name__ == "__main__":
