61ae81bddf760c237585982001960b86780e7f8f
Ching Fang <cfang@holygpu8a15202.rc.fas.harvard.edu>
2025-01-12 14:36:11 -0500

    Add HDF5 option to dataloaders and data collectiong

---
+++ collect_data_h5.py
@@ -0,0 +1,277 @@
+import argparse
+import os
+import pickle
+import random
+import h5py
+
+import gym
+import numpy as np
+from skimage.transform import resize
+from IPython import embed
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+from src.envs.darkroom_env import DarkroomEnv
+from src.envs.maze_env import MazeEnv
+from src.envs.trees import TreeEnv
+from src.utils import (
+    build_env_name,
+    build_dataset_name,
+)
+
+
+def generate_history(env, rollin_type):
+    """ Makes a trajectory from an environment. """
+    states = []
+    actions = []
+    next_states = []
+    rewards = []
+
+    state = env.sample_state()
+    if rollin_type == 'explore':
+        env.update_exploration_buffer(None, state)
+    for _ in range(env.horizon):
+        if rollin_type == 'uniform':
+            state = env.sample_state()
+            action = env.sample_action()
+        elif rollin_type == 'expert':
+            action = env.opt_action(state)
+        elif rollin_type == 'explore':
+            action = env.explore_action()
+        else:
+            raise NotImplementedError
+        next_state, reward = env.transit(state, action)
+
+        if rollin_type == 'explore':
+            env.update_exploration_buffer(action, next_state)
+
+        states.append(state)
+        actions.append(action)
+        next_states.append(next_state)
+        rewards.append(reward)
+        state = next_state
+
+    states = np.array(states)
+    actions = np.array(actions)
+    next_states = np.array(next_states)
+    rewards = np.array(rewards)
+
+    return states, actions, next_states, rewards
+
+def generate_multiple_histories(env_class, env_configs, rollin_type):
+    """ Makes a list of trajectories from a list of environments. """
+    trajs = []
+    for env_config in env_configs:
+        env = env_class(**env_config)
+        (
+            context_states,
+            context_actions,
+            context_next_states,
+            context_rewards,
+        ) = generate_history(env, rollin_type)
+        query_state = env.sample_state()
+        optimal_action = env.opt_action(query_state)
+        traj = {
+            'query_state': query_state,
+            'optimal_action': optimal_action,
+            'context_states': context_states,
+            'context_actions': context_actions,
+            'context_next_states': context_next_states,
+            'context_rewards': context_rewards,
+            'goal': env.goal,
+        }
+        if env_class == TreeEnv:
+            traj['initialization_seed'] = env.initialization_seed
+        trajs.append(traj)
+    return trajs
+
+
+@hydra.main(version_base=None, config_path="configs", config_name="data_collection")
+def main(cfg: DictConfig):
+    np.random.seed(0)
+    random.seed(0)
+    env_config = OmegaConf.to_container(cfg.env, resolve=True)
+    env_name = build_env_name(env_config)
+    rollin_type = env_config['rollin_type']
+    dataset_storage_dir = f'{cfg.storage_dir}/{cfg.wandb.project}/{env_name}/datasets'
+    os.makedirs(dataset_storage_dir, exist_ok=True)
+
+    if env_config['env'] == 'darkroom':
+        goals = np.array([[(j, i) for i in range(env_config['dim'])]
+                         for j in range(env_config['dim'])]).reshape(-1, 2)
+        n_envs = env_config['n_envs']
+        dim = env_config['dim']
+        n_repeats = n_envs // (dim * dim)
+
+        np.random.RandomState(seed=0).shuffle(goals)
+        split_idx_1 = int(.8 * len(goals))
+        split_idx_2 = int(.9 * len(goals))
+        train_goals = goals[:split_idx_1]
+        test_goals = goals[split_idx_1:split_idx_2]
+        eval_goals = goals[split_idx_2:]
+
+        env_configs = [{
+            'dim': env_config['dim'], 'goal': goal, 'horizon': env_config['horizon']
+            } for goal in np.repeat(train_goals, n_repeats, axis=0)]
+        train_trajs = generate_multiple_histories(DarkroomEnv, env_configs, rollin_type)
+        print('Generated train trajectories.')
+
+        env_configs = [{
+            'dim': env_config['dim'], 'goal': goal, 'horizon': env_config['horizon']
+            } for goal in np.repeat(test_goals, n_repeats, axis=0)]
+        test_trajs = generate_multiple_histories(DarkroomEnv, env_configs, rollin_type)
+        print('Generated test trajectories.')
+
+        env_configs = [{
+            'dim': env_config['dim'], 'goal': goal, 'horizon': env_config['horizon']
+            } for goal in np.repeat(eval_goals, n_repeats, axis=0)]
+        eval_trajs = generate_multiple_histories(DarkroomEnv, env_configs, rollin_type)
+        print('Generated eval trajectories.')
+
+    elif env_config['env'] == 'maze':
+        layers = env_config['layers']
+        goals = [(layers-1, p) for p in range(2**(layers-1))]
+        n_envs = env_config['n_envs']
+        n_repeats = n_envs // (len(goals))
+
+        np.random.RandomState(seed=0).shuffle(goals)
+        split_idx_1 = int(.8 * len(goals))
+        split_idx_2 = int(.9 * len(goals))
+        train_goals = goals[:split_idx_1]
+        test_goals = goals[split_idx_1:split_idx_2]
+        eval_goals = goals[split_idx_2:]
+
+        env_configs = [{
+            'layers': env_config['layers'], 'goal': goal, 'horizon': env_config['horizon']
+            } for goal in np.tile(train_goals, (n_repeats, 1))]
+        train_trajs = generate_multiple_histories(MazeEnv, env_configs, rollin_type)
+        print('Generated train trajectories.')
+
+        env_configs = [{
+            'layers': env_config['layers'], 'goal': goal, 'horizon': env_config['horizon']
+            } for goal in np.tile(test_goals, (n_repeats, 1))]
+        test_trajs = generate_multiple_histories(MazeEnv, env_configs, rollin_type)
+        print('Generated test trajectories.')
+
+        env_configs = [{
+            'layers': env_config['layers'], 'goal': goal, 'horizon': env_config['horizon']
+            } for goal in np.tile(eval_goals, (n_repeats, 1))]
+        eval_trajs = generate_multiple_histories(MazeEnv, env_configs, rollin_type)
+        print('Generated eval trajectories.')
+
+    elif (env_config['env'] == 'tree') and (env_config['branching_prob'] == 1.):
+        layers = env_config['max_layers']
+        n_envs = env_config['n_envs']
+        goals = [(layers-1, p) for p in range(2**(layers-1))]
+        n_repeats = n_envs // (len(goals))
+
+        np.random.RandomState(seed=0).shuffle(goals)
+        split_idx_1 = int(.8 * len(goals))
+        split_idx_2 = int(.9 * len(goals))
+        train_goals = goals[:split_idx_1]
+        test_goals = goals[split_idx_1:split_idx_2]
+        eval_goals = goals[split_idx_2:]
+
+        env_configs = [{
+            'max_layers': env_config['max_layers'],
+            'initialization_seed': i,
+            'horizon': env_config['horizon'],
+            'branching_prob': env_config['branching_prob'],
+            'node_encoding': env_config['node_encoding'],
+            'goal': goal
+            } for i, goal in enumerate(np.tile(train_goals, (n_repeats, 1)))]
+        train_trajs = generate_multiple_histories(TreeEnv, env_configs, rollin_type)
+        print('Generated train trajectories.')
+
+        env_configs = [{
+            'max_layers': env_config['max_layers'],
+            'initialization_seed': i,
+            'horizon': env_config['horizon'],
+            'branching_prob': env_config['branching_prob'],
+            'node_encoding': env_config['node_encoding'],
+            'goal': goal
+            } for i, goal in enumerate(np.tile(test_goals, (n_repeats, 1)))]
+        test_trajs = generate_multiple_histories(TreeEnv, env_configs, rollin_type)
+        print('Generated test trajectories.')
+
+        env_configs = [{
+            'max_layers': env_config['max_layers'],
+            'initialization_seed': i,
+            'horizon': env_config['horizon'],
+            'branching_prob': env_config['branching_prob'],
+            'node_encoding': env_config['node_encoding'],
+            'goal': goal
+            } for i, goal in enumerate(np.tile(eval_goals, (n_repeats, 1)))]
+        eval_trajs = generate_multiple_histories(TreeEnv, env_configs, rollin_type)
+        print('Generated eval trajectories.')
+
+    elif (env_config['env'] == 'tree') and (env_config['branching_prob'] != 1.):
+        unique_seeds_path = dataset_storage_dir + '/unique_seeds.pkl'
+        n_envs = env_config['n_envs']
+        with open(unique_seeds_path, 'rb') as f:
+            unique_seeds = pickle.load(f)
+        train_seeds = unique_seeds['train']
+        test_seeds = unique_seeds['test']
+        eval_seeds = unique_seeds['eval']
+        n_unique_seeds = len(train_seeds) + len(test_seeds) + len(eval_seeds)
+        n_repeats = max(n_envs // n_unique_seeds, 1)
+        print(f"n_repeats: {n_repeats}")
+
+        env_configs = [{
+            'max_layers': env_config['max_layers'],
+            'initialization_seed': s,
+            'horizon': env_config['horizon'],
+            'branching_prob': env_config['branching_prob'],
+            'node_encoding': env_config['node_encoding']
+            } for s in np.tile(train_seeds, (n_repeats))]
+        train_trajs = generate_multiple_histories(TreeEnv, env_configs, rollin_type)
+        print('Generated train trajectories.')
+
+        env_configs = [{
+            'max_layers': env_config['max_layers'],
+            'initialization_seed': s,
+            'horizon': env_config['horizon'],
+            'branching_prob': env_config['branching_prob'],
+            'node_encoding': env_config['node_encoding']
+            } for s in np.tile(test_seeds, (n_repeats))]
+        test_trajs = generate_multiple_histories(TreeEnv, env_configs, rollin_type)
+        print('Generated test trajectories.')
+
+        env_configs = [{
+            'max_layers': env_config['max_layers'],
+            'initialization_seed': s,
+            'horizon': env_config['horizon'],
+            'branching_prob': env_config['branching_prob'],
+            'node_encoding': env_config['node_encoding']
+            } for s in np.tile(eval_seeds, (n_repeats))]
+        eval_trajs = generate_multiple_histories(TreeEnv, env_configs, rollin_type)
+        print('Generated eval trajectories.')
+
+    else:
+        raise NotImplementedError
+    
+
+    train_filepath = os.path.join(dataset_storage_dir, 'train.h5')
+    save_to_hdf5(train_filepath, train_trajs)
+    print(f"Saved to {train_filepath}.")
+
+    test_filepath = os.path.join(dataset_storage_dir, 'test.h5')
+    save_to_hdf5(test_filepath, test_trajs)
+    print(f"Saved to {test_filepath}.")
+
+    eval_filepath = os.path.join(dataset_storage_dir, 'eval.h5')
+    save_to_hdf5(eval_filepath, eval_trajs)
+    print(f"Saved to {eval_filepath}.")
+
+
+def save_to_hdf5(filepath, data):
+    if os.path.exists(filepath):
+        os.remove(filepath)
+    with h5py.File(filepath, 'w') as h5file:
+        for idx, entry in enumerate(data):
+            group = h5file.create_group(str(idx))
+            for key, value in entry.items():
+                group.create_dataset(key, data=value)
+
+if __name__ == "__main__":
+    main()
--- src/dataset.py
+++ src/dataset.py
@@ -1,13 +1,88 @@
 import pickle
-
+import h5py
 import numpy as np
 import torch
+import sys
+import signal
 
 from src.utils import convert_to_tensor
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+def get_dataset(path, config):
+    if path.lower().endswith(('.h5', '.hdf5', '.he5')):
+        return HDF5Dataset(path, config)
+    else:
+        return Dataset(path, config)
+    
+class HDF5Dataset(torch.utils.data.Dataset):
+    _open_files = set()
 
+    def __init__(self, path, config):
+        self.h5_path = path
+        self._file = None
+        self.horizon = config['horizon']
+        with h5py.File(path, 'r') as f:
+            self.length = len(f.keys())
+            self.indices = list(f.keys())
+        self.epoch = 0
+        self.zeros = np.zeros(config['state_dim'] ** 2 + config['action_dim'] + 1)
+        self.zeros = convert_to_tensor(self.zeros)
+
+        self._setup_signal_handlers()
+    
+    @classmethod
+    def _setup_signal_handlers(cls):
+        # Only set up handlers once
+        if not hasattr(cls, '_handlers_setup'):
+            signal.signal(signal.SIGINT, cls._signal_handler)
+            signal.signal(signal.SIGTERM, cls._signal_handler)
+            cls._handlers_setup = True
+
+    @classmethod
+    def _signal_handler(cls, signum, frame):
+        """Handle cleanup when receiving a signal"""
+        print("\nClosing HDF5 files...")
+        for file in cls._open_files:
+            try:
+                file.close()
+            except Exception as e:
+                print(f"Error closing file: {e}")
+        cls._open_files.clear()
+        sys.exit(1) # Re-raise the signal
+
+    def shuffle_indices(self):
+        np.random.shuffle(self.indices)
+        self.epoch += 1
+    
+    def __getitem__(self, idx):
+        if self._file is None:
+            self._file = h5py.File(self.h5_path, 'r')
+            self._open_files.add(self._file)
+
+        shuffled_idx = self.indices[idx]
+        entry = self._file[shuffled_idx]
+        res = {
+            'context_states': convert_to_tensor(entry['context_states']),
+            'context_actions': convert_to_tensor(entry['context_actions']),
+            'context_next_states': convert_to_tensor(entry['context_next_states']),
+            'context_rewards': convert_to_tensor(entry['context_rewards'])[:, None],
+            'query_states': convert_to_tensor(entry['query_state']),
+            'optimal_actions': convert_to_tensor(entry['optimal_action']),
+            'zeros': self.zeros,
+        }
+
+        return res
+
+    def __len__(self):
+        return self.length
+
+    def __del__(self):
+        if self._file is not None:
+            self._file.close()
+            self._open_files.discard(self._file)
+
+        
 class Dataset(torch.utils.data.Dataset):
     """Dataset class."""
 
@@ -37,7 +112,6 @@ class Dataset(torch.utils.data.Dataset):
             context_actions.append(traj['context_actions'])
             context_next_states.append(traj['context_next_states'])
             context_rewards.append(traj['context_rewards'])
-
             query_states.append(traj['query_state'])
             optimal_actions.append(traj['optimal_action'])
 
@@ -75,52 +149,6 @@ class Dataset(torch.utils.data.Dataset):
             'context_actions': self.dataset['context_actions'][index],
             'context_next_states': self.dataset['context_next_states'][index],
             'context_rewards': self.dataset['context_rewards'][index],
-            'query_states': self.dataset['query_states'][index],
-            'optimal_actions': self.dataset['optimal_actions'][index],
-            'zeros': self.zeros,
-        }
-
-        return res
-
-
-class ImageDataset(Dataset):
-    """"Dataset class for image-based data."""
-
-    def __init__(self, paths, config, transform):
-        config['store_gpu'] = False
-        super().__init__(paths, config)
-        self.transform = transform
-        self.config = config
-
-        context_filepaths = []
-        query_images = []
-
-        for traj in self.trajs:
-            context_filepaths.append(traj['context_images'])
-            query_image = self.transform(traj['query_image']).float()
-            query_images.append(query_image)
-
-        self.dataset.update({
-            'context_filepaths': context_filepaths,
-            'query_images': torch.stack(query_images),
-        })
-
-    def __getitem__(self, index):
-        'Generates one sample of data'
-        filepath = self.dataset['context_filepaths'][index]
-        context_images = np.load(filepath)
-        context_images = [self.transform(images) for images in context_images]
-        context_images = torch.stack(context_images).float()
-
-        query_images = self.dataset['query_images'][index]
-
-        res = {
-            'context_images': context_images,#.to(device),
-            'context_states': self.dataset['context_states'][index],
-            'context_actions': self.dataset['context_actions'][index],
-            'context_next_states': self.dataset['context_next_states'][index],
-            'context_rewards': self.dataset['context_rewards'][index],
-            'query_images': query_images,#.to(device),
             'query_states': self.dataset['query_states'][index],
             'optimal_actions': self.dataset['optimal_actions'][index],
             'zeros': self.zeros,
