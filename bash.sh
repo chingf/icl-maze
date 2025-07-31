c58b0cfbeeec04eec61076994b7563c647f81b68
Ching Fang <chingfang17@gmail.com>
2025-07-31 10:22:00 -0400

    Remove old bash script

--- bash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python train.py model.initialization_seed=0 optimizer.use_scheduler=False optimizer.lr=1e-5 optimizer.num_epochs=100
-python train.py model.initialization_seed=1 optimizer.use_scheduler=False optimizer.lr=1e-5 optimizer.num_epochs=100
-python train.py model.initialization_seed=2 optimizer.use_scheduler=False optimizer.lr=1e-5 optimizer.num_epochs=100
